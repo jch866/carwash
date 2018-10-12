9e5621f71547f60ffcedce4ceb763f1b33d9bb9a
jch866 <jch866@126.com>
2018-10-12 18:09:33 +0800

    洗车记录

--- src/pages/record.vue
+++ src/pages/record.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-record
+    <div class="record">
+      <div class="record_item" v-for='item in orders' :key='item.id'>
+        <p>订单号：{{item.num}}</p>
+        <p>网点：{{item.net}}</p>
+        <p>套餐：{{item.type}}</p>
+        <p>时间：{{item.time}}</p>
+        <p>状态：{{item.status}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import util from "../utils";
 export default {
     data(){
+      let config = util.config;
         return {
-
+          orders:config.orders,
         }
     },
     methods:{
--- src/utils/config.js
+++ src/utils/config.js
@@ -8,7 +8,50 @@ const cfg = {
     { time: '15分钟', value: '1', price: '1元', index: 0 },
     { time: '30分钟', value: '2', price: '2元', index: 1 },
     { time: '45分钟', value: '3', price: '3元', index: 2 }
-  ]
+  ],
+  orders:[{
+    id:'1',
+    num:'1536726373436UTGhlJi',
+    time:'2018-10-12 12:26:13',
+    status:'待支付',
+    net:'碧云湾',
+    type:'1元15分钟'
+  },{
+    id:'2',
+    num:'1536726373436UTGhlJi',
+    time:'2018-10-12 12:26:13',
+    status:'订单成功',
+    net:'碧云湾',
+    type:'3元45分钟'
+  },{
+    id:'3',
+    num:'1536726373436UTGhlJi',
+    time:'2018-10-12 12:26:13',
+    status:'订单失效',
+    net:'碧云湾',
+    type:'1元15分钟'
+  },{
+    id:'4',
+    num:'1536726373436UTGhlJi',
+    time:'2018-10-12 12:26:13',
+    status:'订单撤销',
+    net:'碧云湾',
+    type:'1元15分钟'
+  },{
+    id:'5',
+    num:'1536726373436UTGhlJi',
+    time:'2018-10-12 12:26:13',
+    status:'支付成功',
+    net:'碧云湾',
+    type:'1元15分钟'
+  },{
+    id:'6',
+    num:'1536726373436UTGhlJi',
+    time:'2018-10-12 12:26:13',
+    status:'订单失败',
+    net:'碧云湾',
+    type:'1元15分钟'
+  }]
 }
 
 export default cfg;
