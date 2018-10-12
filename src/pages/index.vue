dcb6c47c9bff0335637399563adac529c0e1f2c2
jch866 <jch866@126.com>
2018-10-12 17:31:49 +0800

    修改选中时的UI;利用伪类选定

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,16 +8,16 @@
     <mt-tab-container v-model="selected">
       <mt-tab-container-item id="1">
         <section class="content_top">
-          <div class="adv_banner"><img src="" alt=""></div>
+          <div class="adv_banner"><img src="../assets/banner.jpg"></div>
           <div class="check_title">洗车时长选择</div>
           <my-check-suit :option='config.suits' v-model='checkedVal' @change='setselectVal'></my-check-suit>
           <div class="check_note">
-            <p>备注:</p>
+            <p class="orange">备注:</p>
             <p>支付成功以后须在30分钟内启动设备，否则订单失效，失效的订单无法退款，请知悉！</p>
           </div>
         </section>
         <div class='bottom_pay'>
-          <span class="pay_info">订单金额：{{`¥${checkedVal}.00元`}}</span>
+          <span class="pay_info">订单金额：<span class="pay_amount">{{`¥${checkedVal}.00元`}}</span></span>
           <span class="pay_btn">微信支付</span>
         </div>
       </mt-tab-container-item>
@@ -37,7 +37,7 @@ export default {
     let config = util.config;
     return {
       selected: "1",
-      checkedVal:'2',
+      checkedVal:'1',//默认套餐
       config,
       api: config.index.url,
     }
